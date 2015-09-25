b26c81d30a1dbcbc03bb77dc8c2d5f15f7291750
Syam  S Pillai <syam.sp0965@gmail.com>
2015-09-25 16:41:26 +0530

    worked on POST method

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var dns = require('dns');
 var express = require('express');
 var gulp = require('gulp');
 var status = require('./status.json');
-
+var fs = require('fs');
 const PORT=8888;
 
 function handleRequest(request, response){
@@ -11,7 +11,9 @@ function handleRequest(request, response){
 }
 console.log(status.first + ' '+ status.middle + ' '+ status.last);
 var server = http.createServer(handleRequest);
-
+fs.readFile('./smthng.txt','utf8',function (err, data) {
+    console.log(data);
+});
 server.listen(PORT, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
 });
